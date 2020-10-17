09dbe06bd9fbd2bacb7a546a130d788ac840241b
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-17 12:08:03 +0700

    IF1-10119011-Latihan10-Logika

---
+++ src/if1/pkg10119011/latihan10/logika/IF110119011Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan10.logika;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+    
+}
